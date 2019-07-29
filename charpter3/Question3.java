6b8fd51d73860a2ec76da9deea3f8e17f72c5efb
流氓兔 <wangwei_ww>
2019-07-29 17:00:43 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

---
+++ charpter3/Question3.java
@@ -0,0 +1,17 @@
+public class Question3 {
+	public static void main(String[] args) {
+		int a = 9527;
+		int c = 1;
+		for (int b = a; b > 10; b /= 10) {
+			c *= 10;
+		}
+		for (; c > 0; c /= 10) {
+			int d = a;
+			d /= c;
+			String str = "" + d;
+			System.out.print(" " + str);
+			a %= c;
+		}
+
+	}
+}
