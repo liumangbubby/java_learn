cac4cfc26266802330254e4c800f5b743c4e7f45
li Hao <1321443326@qq.com>
2019-07-30 16:25:55 +0800

    charpter4 finish

---
+++ src/java/charpter4/Question2.java
@@ -0,0 +1,15 @@
+package charpter4;
+
+public class Question2 {
+    public static void main(String[] args) {
+        for(int a = 100;a<1000;a++){
+            int a1 = a / 100;
+            int a2 = (a%100)/10;
+            int a3 = a% 10;
+            if(a == a1 * a1*a1+a2*a2*a2+a3*a3*a3){
+                System.out.print(a);
+                System.out.print(",");
+            }
+        }
+    }
+}
